36b50759a86127fd80ccf0c55c16485ee2b2391b
Alex Homer <88427828+AlexJHomer@users.noreply.github.com>
2021-08-19 11:02:44 +0100

    Modify to use updated version of unvotes dataset

--- data/unvotes.R
+++ data/unvotes.R
@@ -7,15 +7,9 @@ library(here)
 # unvotes ----------------------------------------------------------------------
 
 unvotes <- un_votes %>%
-  mutate(country =
-           case_when(
-             country == "United Kingdom of Great Britain and Northern Ireland" ~ "UK & NI",
-             country == "United States of America"                             ~ "US",
-             TRUE                                                              ~ country
-           )) %>%
   inner_join(un_roll_calls, by = "rcid") %>%
   inner_join(un_roll_call_issues, by = "rcid")
 
 # save RDS ---------------------------------------------------------------------
 
-saveRDS(unvotes, file = here::here("01-whole-game/data/unvotes.rds"))
+saveRDS(unvotes, file = here::here("data/unvotes.rds"))
